afd6c8857bdeca8308211afc6f88ada85f89be24
ziemian <mziemianek3@gmail.com>
2026-04-15 09:14:36 +0200

    Save local lab scripts before flux bootstrap

--- scripts/00-provision-cluster.sh
+++ scripts/00-provision-cluster.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 # Check if the --with-proxy flag is provided
 for arg in "$@"; do
   if [ "$arg" == "--with-proxy" ]; then
-    kind create cluster --config ../configs/cluster-config-with-proxy.yaml
+    kind create cluster --config ./configs/cluster-config-with-proxy.yaml
     exit $?
   fi
 done
 
-kind create cluster --config ../configs/cluster-config.yaml
+kind create cluster --config ./configs/cluster-config.yaml
--- scripts/20-setup-cilium.sh
+++ scripts/20-setup-cilium.sh
@@ -3,14 +3,17 @@ set -euo pipefail
 
 CILIUM_NS=${1:-"cilium"}
 
-# TODO: get ip dynamically
-API_SERVER_IP="172.18.0.3"
+# Get the name of the control-plane container
+CONTROL_PLANE_NAME=$(docker ps --filter "label=io.x-k8s.kind.role=control-plane" --format "{{.Names}}")
+
+# Get its internal Docker IP
+API_SERVER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${CONTROL_PLANE_NAME}")
 API_SERVER_PORT=6443
 
 kubectl create namespace "$CILIUM_NS"
 
-helm install cilium ../helm/charts/cilium \
-    --values ../helm/values/cilium.yaml \
+helm install cilium ./helm/charts/cilium \
+    --values ./helm/values/cilium.yaml \
     --namespace ${CILIUM_NS} \
     --set k8sServiceHost=${API_SERVER_IP} \
     --set k8sServicePort=${API_SERVER_PORT}
